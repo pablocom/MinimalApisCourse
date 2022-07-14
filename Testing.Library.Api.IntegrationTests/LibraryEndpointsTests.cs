1f31352d17b05549e1db8d58c36bd76cf8bd7074
Pablo <p.c.ramirez5150@gmail.com>
2022-07-14 18:50:02 +0200

    Removes created resources

--- Testing.Library.Api.IntegrationTests/LibraryEndpointsTests.cs
+++ Testing.Library.Api.IntegrationTests/LibraryEndpointsTests.cs
@@ -4,9 +4,10 @@ using Testing.Library.Api.Models;
 
 namespace Testing.Library.Api.IntegrationTests;
 
-public class LibraryEndpointsTests : IClassFixture<WebApplicationFactory<ILibraryApiMarker>>
+public class LibraryEndpointsTests : IClassFixture<WebApplicationFactory<ILibraryApiMarker>>, IAsyncLifetime
 {
     private readonly WebApplicationFactory<ILibraryApiMarker> _factory;
+    private readonly List<string> _createdIsbns = new();
 
     public LibraryEndpointsTests(WebApplicationFactory<ILibraryApiMarker> factory)
     {
@@ -21,10 +22,44 @@ public class LibraryEndpointsTests : IClassFixture<WebApplicationFactory<ILibrar
 
         var result = await httpClient.PostAsJsonAsync("/books", book);
         var createdBook = await result.Content.ReadFromJsonAsync<Book>();
+        _createdIsbns.Add(createdBook!.Isbn);
 
         result.StatusCode.Should().Be(HttpStatusCode.Created);
         createdBook.Should().BeEquivalentTo(book);
         result.Headers.Location!.PathAndQuery.Should().Be($"/books/{book.Isbn}");
+    }
+
+    [Fact]
+    public async Task CreateBook_Fails_WhenIsbnIsInvalid()
+    {
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+        book.Isbn = "INVALID";
+
+        var result = await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book!.Isbn);
+        var error = (await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>())!.Single();
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Isbn");
+        error.ErrorMessage.Should().Be("Value was not a valid ISBN-13");
+    }
+
+    [Fact]
+    public async Task CreateBook_Fails_WhenBookExists()
+    {
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book!.Isbn);
+        var result = await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book!.Isbn);
+        var error = (await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>())!.Single();
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Isbn");
+        error.ErrorMessage.Should().Be("A book with this ISBN-13 already exists");
     }
 
     private Book GenerateBook(string title = "The Dirty Coder")
@@ -47,5 +82,16 @@ public class LibraryEndpointsTests : IClassFixture<WebApplicationFactory<ILibrar
             $"{Random.Shared.Next(10000, 99999)}-" +
             $"{Random.Shared.Next(100, 999)}-" +
             $"{Random.Shared.Next(0, 9)}";
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public async Task DisposeAsync()
+    {
+        var httpClient = _factory.CreateClient();
+        foreach (var isbn in _createdIsbns)
+        {
+            await httpClient.DeleteAsync($"/books/{isbn}");
+        }
     }
 }
---
+++ Testing.Library.Api.IntegrationTests/ValidationError.cs
@@ -0,0 +1,7 @@
+﻿namespace Testing.Library.Api.IntegrationTests;
+
+public class ValidationError
+{
+    public string PropertyName { get; set; } = default!;
+    public string ErrorMessage { get; set; } = default!;
+}
